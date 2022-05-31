c3aadecf894575696e5ffe1871dad1aee1c51dfb
callistatjonadi <97695104+callistatjonadi@users.noreply.github.com>
2022-05-31 22:50:17 +0700

    Prisma Segitiga

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+
+import java.util.Scanner;
+
+import bangunruang.PrismaSegitiga;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner keyboard = new Scanner(System.in);
+    
+        PrismaSegitiga prismaSegitiga = new PrismaSegitiga();
+        System.out.println("=====================================================================================================");
+        System.out.println("Program Menghitung Volume Prisma Segitiga");
+        System.out.println("=====================================================================================================");
+        System.out.print("Masukkan Alas (a) (cm) = ");
+        prismaSegitiga.a = keyboard.nextInt();
+        System.out.print("Masukkan Tinggi (t) (cm) = ");
+        prismaSegitiga.t = keyboard.nextInt();
+        System.out.print("Masukkan TInggi Prisma (cm) = ");
+        prismaSegitiga.tinggi = keyboard.nextInt();
+        System.out.println("=====================================================================================================");
+        prismaSegitiga.tampilkanVolumePrismaSegitiga();
     }
 }
---
+++ src/bangunruang/PrismaSegitiga.java
@@ -0,0 +1,17 @@
+package bangunruang;
+public class PrismaSegitiga {
+
+    public int a = 0;
+    public int t = 0;
+    public int tinggi = 0;
+    public float volume = 0;
+
+    public void hitungVolumePrismaSegitiga(){
+        volume = ((float)1/2 * a * t) * tinggi;
+    }
+
+    public void tampilkanVolumePrismaSegitiga(){
+        hitungVolumePrismaSegitiga();
+        System.out.println("Volume Prisma Segitiga = " + volume + " cm3");
+    }
+}
